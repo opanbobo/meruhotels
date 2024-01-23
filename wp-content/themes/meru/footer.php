92c30691b6fdd94fea3cfd1de276dd34c9c79219
Tofan <tofan.rukmana@bankina.id>
2024-01-23 20:11:36 +0700

    update layout footer

---
+++ wp-content/themes/meru/footer.php
@@ -0,0 +1,106 @@
+<div id="footer" class="py-5">
+    <div class="container">
+      <div class="title-section text-center">
+        <h1>Join Our Exclusive Mailing List for a Refined Experience</h1>
+      </div>
+      <div class="subtitle text-center">
+        <p>Your visit has been truly delightful and we can't wait to enhance your next stay even further. Subscribe to our mailing list for a world of elevated experiences awaiting you.</p>
+      </div>
+      <div class="form text-center">
+        <form>
+          <div class="form-group">
+            <input type="text" class="form-control" id="" placeholder="Enter email">
+          </div>
+        </form>
+      </div>
+      <div class="after-form text-center">
+        <p>By subscribing, you are agreeing to our terms and conditions. See privacy policy for more.</p>
+      </div>
+      <div class="footer-menus-wrapper border-top py-5">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="footer-menu-left d-flex">
+              <div class="footer-block col-md-2">
+                <p class="text-uppercase title-col">about us</p>
+                <ul class="text-uppercase">
+                  <li><a href="#">philosophy</a></li>
+                  <li><a href="#">Directions</a></li>
+                  <li><a href="#">faq</a></li>
+                  <li><a href="#">contact us</a></li>
+                </ul>
+              </div>
+              <div class="footer-block  col-md-2">
+                <p class="text-uppercase title-col">Hotel & resorts</p>
+                <ul class="text-uppercase">
+                  <li><a href="#">The Meru Hotel</a></li>
+                  <li><a href="#">The Meru Resort</a></li>
+                </ul>
+              </div>
+              <div class="footer-block  col-md-2">
+                <p class="text-uppercase title-col">Restaurant</p>
+                <ul class="text-uppercase">
+                  <li><a href="#">arupadatu</a></li>
+                  <li><a href="#">sūkla</a></li>
+                </ul>
+              </div>
+              <div class="footer-block  col-md-2">
+                <p class="text-uppercase title-col">Spa & Wellness</p>
+                <ul class="text-uppercase">
+                  <li><a href="#">Gringsing</a></li>
+                  <li><a href="#">Sudamala</a></li>
+                </ul>
+              </div>
+              <div class="footer-block  col-md-2">
+                <p class="text-uppercase title-col">convention center</p>
+                <ul class="text-uppercase">
+                  <li><a href="#">Agung</a></li>
+                </ul>
+              </div>
+              <div class="footer-block  col-md-2">
+                <p class="text-uppercase title-col">Others Facilities</p>
+                <ul class="text-uppercase">
+                  <li><a href="#">Kids Club</a></li>
+                  <li><a href="#">Retail</a></li>
+                  <li><a href="#">Beachfront Lounge</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="footer-right">
+              <div class="block text-uppercase text-right">
+                <p class="title-col-large mb-0">THE MERU sanur</p>
+                <p class="mb-0">Jalan Hang Tuah 
+                Sanur Kaja, Denpasar Selatan,
+                Kota Denpasar, Bali 8022</p>
+                <p>Phone: (0361) 288511</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom d-flex justify-content-between">
+        <div class="footer-bottom-left d-flex align-items-center">
+          <a href="#" class="mr-5"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo-meru-bottom.png" alt="" class="img-fluid"></a>
+          <a href="#" class="pt-5"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo-bumn-footer.png" alt="" class="img-fluid"></a>
+        </div>
+        <div class="footer-bottom-right d-flex align-items-center">
+          <ul class="mb-0 mt-5">
+            <li><a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/img/ico-twt.png" alt="" class="img-fluid"></a></li>
+            <li><a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/img/ico-fb.png" alt="" class="img-fluid"></a></li>
+            <li><a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/img/ico-tiktok.png" alt="" class="img-fluid"></a></li>
+            <li><a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/img/ico-ig.png" alt="" class="img-fluid"></a></li>
+            <li><a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/img/ico-yt.png" alt="" class="img-fluid"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/assets/vendor/select2/js/select2.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/assets/vendor/slick/js/slick.min.js"></script>
+  </div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/meru/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php wp_title('-', true, 'right'); ?></title>
+
+  <!-- Vendor CSS Files -->
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/vendor/select2/css/select2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/vendor/slick/css/slick.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/vendor/slick/css/slick-theme.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/main.css">
+  
+  <?php wp_head(); ?>
+</head>
+<body >
