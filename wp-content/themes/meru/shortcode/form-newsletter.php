72be6640174491c4cf5821669eb044efd56c8994
Tofan <tofan.rukmana@bankina.id>
2024-01-24 10:28:12 +0700

    upadte footer

--- wp-content/themes/meru/shortcode/form-newsletter.php
+++ wp-content/themes/meru/shortcode/form-newsletter.php
@@ -9,11 +9,12 @@ function rk_form_newsletter( $atts, $content = null ) {
 		'posts_per_page' => 8,
 	], $atts );
 
-    $output  = '<div class="form text-center">';
-    $output .= '<form>';
-    $output .= '<div class="form-group">';
+    $output  = '<div class="form text-center my-5">';
+    $output .= '<form class="form-subs form-inline">';
+    $output .= '<div class="form-group w-70">';
     $output .= '<input type="text" class="form-control" id="" placeholder="Enter email">';
     $output .= '</div>';
+    $output .= '<button type="submit" class="btn btn-primary btn-submit-subs mb-2 ml-4 px-4 py-1 text-uppercase">Submit</button>';
     $output .= '</form>';
     $output .= '</div>';
 
