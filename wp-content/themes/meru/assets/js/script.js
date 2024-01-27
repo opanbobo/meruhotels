50924ea8a6fb216673103ac2832d1df52c01369c
Fadhel Afif <deel.mr0@gmail.com>
2024-01-27 18:03:12 +0700

    add scroll width effect

--- wp-content/themes/meru/assets/js/script.js
+++ wp-content/themes/meru/assets/js/script.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function($) {
+    var lenis = new Lenis();
     var $window = $(window);
+    var winHeight = $window.height();
+    var winWidth = $window.width();
     var $container = $('.container').first();
     var containerLeft = $container[0].getBoundingClientRect().left;
 
@@ -33,4 +36,60 @@ jQuery(document).ready(function($) {
 
 
 
+    var $scrollWidth = $('.rk-scroll-width');
+
+    function scrollWidthOnScroll() {
+
+        if (winWidth <= 768) return;
+
+        $scrollWidth.each(function(i,e){
+            var $e = $(e);
+            var eRect = e.getBoundingClientRect();
+            var eRectTop = eRect.top;
+            var eRectBottom = eRect.bottom;
+            var eRectHeight = eRect.height;
+
+
+
+            if (eRectTop <= winHeight && eRectTop > winHeight / 2 ) {
+                var progress = (2 * eRectTop - winHeight) / winHeight;
+                // console.info(progress);
+                var margin = 80 * progress;
+
+                $e.css({'margin': '0 ' + margin + 'px'});
+            }
+
+            if (eRectBottom > 0 && eRectBottom <= winHeight / 2 ) {
+                var progress = 1 - (2 * eRectBottom / winHeight);
+                console.info(progress);
+                var margin = 80 * progress;
+
+                $e.css({'margin': '0 ' + margin + 'px'});
+            }
+        })
+    }
+
+    function onScroll(e) {
+        // stickyMenuOnScroll();
+        scrollWidthOnScroll();
+    }
+
+    function onResize() {
+        winHeight = $window.height();
+        winWidth = $window.width();
+    }
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    onScroll();
+    onResize();
+
+    lenis.on('scroll', onScroll);
+    // $window.on('scroll', onScroll);
+    $window.on('resize', onResize);
 });
