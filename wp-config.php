36f2a39121c0e3faf5a40179ad3dc36a52fa2924
Tofan <tofan.rukmana@bankina.id>
2024-01-27 11:41:31 +0700

    update config

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'meruhotels' );
+define( 'DB_NAME', 'u963765151_merulocal' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u963765151_merulocal' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'meru123@B3Rakkk' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '153.92.15.5' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
