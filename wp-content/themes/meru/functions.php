f8924a7b11ab151eab09c0aafecaf2f065e4cc88
Tofan <tofan.rukmana@bankina.id>
2024-03-06 06:09:21 +0700

    nambah tag gtm </head?

--- wp-content/themes/meru/functions.php
+++ wp-content/themes/meru/functions.php
@@ -78,7 +78,13 @@ add_filter( 'image_size_names_choose', 'rk_image_size_names_choose');
 
 function rk_gsc_insert() {
 ?><!-- Google Search Console Meta Tag Code -->
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TFB25TQ3');</script>
+<!-- End Google Tag Manager -->
 <!-- End Google Search Console Meta Tag Code -->
 <?php
 }
