31e41ba3c0681195fb50a44a5d2ca38c4f6f8a93
Fadhel Afif <deel.mr0@gmail.com>
2024-07-02 08:34:34 +0700

    sucuri log

--- wp-content/uploads/sucuri/sucuri-hookdata.php
+++ wp-content/uploads/sucuri/sucuri-hookdata.php
@@ -1,6 +1,6 @@
 <?php
 // datastore=hookdata;
 // created_on=1719026342;
-// updated_on=1719116171;
+// updated_on=1719871619;
 exit(0);
 ?>
--- wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -1,2 +1,3 @@
 <?php exit(0); ?>
 {"user_id":1,"user_login":"admin","user_remoteaddr":"127.0.0.1","user_hostname":"example-app.local","user_lastlogin":"2024-06-22 02:57:11"}
+{"user_id":1,"user_login":"admin","user_remoteaddr":"127.0.0.1","user_hostname":"example-app.local","user_lastlogin":"2024-06-29 12:04:24"}
